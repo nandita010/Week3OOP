01e947db3b43336bd0bdbc2661052801cb227e57
NanditaReji-STUDENT <g00463584@atu.ie>
2025-10-01 17:08:45 +0100

    ADDED

---
+++ src/main/java/org/example/main.java
@@ -0,0 +1,24 @@
+package org.example;
+import java.util.Scanner;
+import java.sql.SQLOutput;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Create an instance of a Calculator
+
+        Calc calc = new Calc();
+
+        System.out.println("Simple Calculator-Add Two Numbers");
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+
+        int result= calc.add(a, b);
+        System.out.println("Result:" +result);
+
+        sc.close();
+    }
+}
