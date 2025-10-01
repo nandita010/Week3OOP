3d28bbd6918a66f081394c91639896c162003faf
NanditaReji-STUDENT <g00463584@atu.ie>
2025-10-01 17:43:17 +0100

    DIVIDED

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Calc {
 //Multiplying the two values
-    public int mul(int a,int b){
-        return a*b;
+    public int div(int a,int b){
+        return a/b;
     }
 
 
