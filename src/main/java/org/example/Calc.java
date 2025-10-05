3a89d4e9b58a0e1f5d9f3c01d3e7232e55a1be97
Nandita <g00463584@atu.ie>
2025-10-05 23:26:02 +0100

    all modified

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,10 +1,29 @@
 package org.example;
 
 public class Calc {
-//Power of the two values
-    public int pow(int a,int b){
+    //Adding
+    public int add(int a, int b) {
+        return a + b;
+    }
+//Subtracting
+    public int sub(int a, int b) {
+        return a - b;
+    }
+//Multiplying
+    public int mul(int a, int b) {
+        return a * b;
+    }
+//Dividing
+    public double div(int a, int b) {
+        return (double) a / b;
+    }
+//Modulo
+    public int mod(int a, int b) {
         return a % b;
     }
+    //Power
 
-
+    public double pow(int a, int b) {
+        return Math.pow(a, b);
+    }
 }
--- src/main/java/org/example/main.java
+++ src/main/java/org/example/main.java
@@ -12,14 +12,33 @@ public class main {
         Calc calc = new Calc();
 
         System.out.println("Simple Calculator-Finding Power with Two Numbers");
+        System.out.println("Please choose an operation(+, -, *, /, %, power): ");
+        String operator = sc.next();
         System.out.println("Enter first number: ");
         int a = sc.nextInt();
         System.out.println("Enter second number: ");
         int b = sc.nextInt();
+        double result = 0;
+//Operations using if statement
+        if (operator.equals("+")) {
+            result = calc.add(a, b);
+        } else if (operator.equals("-")) {
+            result = calc.sub(a, b);
+        } else if (operator.equals("*")) {
+            result = calc.mul(a, b);
+        } else if (operator.equals("/")) {
+            result = calc.div(a, b);
+        } else if (operator.equals("%")) {
+            result = calc.mod(a, b);
+        } else if (operator.equals("power")) {
+            result = calc.pow(a, b);
+        } else {
+            System.out.println("Invalid operation, please try again:");
+            sc.close();
+            return;
+        }
 
-        int result= calc.pow(a, b);
-        System.out.println("Result:" +result);
-
+        System.out.println("Result: " + result);
         sc.close();
     }
 }
